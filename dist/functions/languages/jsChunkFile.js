aac6dad41b547ffb4b9a5f884b41547cd97a87e4
davidwrossiter <davewrossiter@gmail.com>
2024-06-25 15:27:44 +0100

    add global chunking

---
+++ dist/functions/languages/jsChunkFile.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jsChunkFile = ({ path }) => {
+    return;
+};
+exports.default = jsChunkFile;
--- index.ts
+++ index.ts
@@ -1,38 +1,19 @@
-#!/usr/bin/env node
-
-import fs from "fs";
-import path from "path";
-import { Command } from "commander";
-const program = new Command();
+#!/usr/bin/env ts-node
 
 import jsChunkDir from "./functions/languages/jsChunkDir";
 import jsChunkFile from "./functions/languages/jsChunkFile";
 import readFile from "./functions/readFiles/readFile";
 import readDir from "./functions/readFiles/readDir";
-
-program
-  .version("1.0.0")
-  .arguments("<directory>")
-  .description("Chunk JavaScript files in a directory")
-  .action(async (directory: string) => {
-    console.log(`Chunking files in directory: ${directory}`);
-  });
-
-const directoryPath = process.argv[2];
-console.log(`Received directory path: ${directoryPath}`);
+import { argv } from "process";
 
 (async () => {
-  const data = await readDir("./test_code/");
-  // console.log(data);
+  const path = argv[2]; // Get the directory path from the command-line arguments
+  if (path) {
+    const data = await readDir(path);
+    console.log(data);
+  } else {
+    console.log("Please provide a directory path.");
+  }
 })();
 
-// (async () => {
-//   const data = await jsChunkDir({ path: "./test_code/test1.ts" });
-//   console.log(data);
-// })();
-
-program.parse(process.argv);
-
 export { jsChunkDir, jsChunkFile };
-
-// Checking if rebase worked
