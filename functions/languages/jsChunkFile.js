7aa6ba140de3fc5743ba8ae307b465f6d5ef6497
davidwrossiter <davewrossiter@gmail.com>
2024-06-25 13:41:36 +0100

    transpile

---
+++ functions/languages/jsChunkFile.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var jsChunkFile = function (_a) {
+    var path = _a.path;
+    return;
+};
+exports.default = jsChunkFile;
