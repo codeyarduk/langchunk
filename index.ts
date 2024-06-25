d7578f384c73451e5fab7711dcdaeac8e1e71c7a
davidwrossiter <davewrossiter@gmail.com>
2024-06-25 13:39:31 +0100

    update extension

--- index.ts
+++ index.ts
@@ -1,26 +1,29 @@
 #!/usr/bin/env node
 
-// import fs from "fs";
-// import path from "path";
-// import { Command } from "commander";
-// const program = new Command();
+import fs from "fs";
+import path from "path";
+import { Command } from "commander";
+const program = new Command();
 
 import jsChunkDir from "./functions/languages/jsChunkDir";
 import jsChunkFile from "./functions/languages/jsChunkFile";
 import readFile from "./functions/readFiles/readFile";
 import readDir from "./functions/readFiles/readDir";
 
-// program
-//   .version("1.0.0")
-//   .arguments("<directory>")
-//   .description("Chunk JavaScript files in a directory")
-//   .action(async (directory: string) => {
-//     console.log(`Chunking files in directory: ${directory}`);
-//   });
+program
+  .version("1.0.0")
+  .arguments("<directory>")
+  .description("Chunk JavaScript files in a directory")
+  .action(async (directory: string) => {
+    console.log(`Chunking files in directory: ${directory}`);
+  });
+
+const directoryPath = process.argv[2];
+console.log(`Received directory path: ${directoryPath}`);
 
 (async () => {
   const data = await readDir("./test_code/");
-  console.log(data);
+  // console.log(data);
 })();
 
 // (async () => {
@@ -28,7 +31,7 @@ import readDir from "./functions/readFiles/readDir";
 //   console.log(data);
 // })();
 
-// program.parse(process.argv);
+program.parse(process.argv);
 
 export { jsChunkDir, jsChunkFile };
 
