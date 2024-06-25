8e916d09b51015b3c11c87ebac6c6fd68a54deed
davidwrossiter <davewrossiter@gmail.com>
2024-06-25 13:46:39 +0100

    remove ts

--- functions/languages/jsChunkFile.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var jsChunkFile = function (_a) {
-    var path = _a.path;
-    return;
-};
-exports.default = jsChunkFile;
