120fd8d2e38a9e39be9ef13566fba167813ebb26
davidwrossiter <davewrossiter@gmail.com>
2024-07-15 08:11:41 +0100

    Removed folder from Git

--- testFiles/pointsOfInterest.ts
+++
@@ -1,7 +0,0 @@
-import readDir from "../functions/readFiles/readDir";
-
-(async () => {
-  const data = await readDir("./testFiles");
-  // console.log(path);
-//   console.log(data.dir_data[0].data_chunks);
-})();
