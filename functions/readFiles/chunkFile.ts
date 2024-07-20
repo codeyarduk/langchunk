a4e3e3bed476be70ba701626b736c79384b0b6cd
davidwrossiter <davewrossiter@gmail.com>
2024-07-20 13:12:14 +0100

    refactor multiple files and add flat structure to data chunks

--- functions/readFiles/chunkFile.ts
+++ functions/readFiles/chunkFile.ts
@@ -45,6 +45,19 @@ const findChunks = ({ node, code, path, languageNodes }: findChunksParams) => {
   const getChunks = (node: any, depth: number = 0) => {
     const listAllowedNodeTypes = languageNodes || [""];
 
+    // console.log("node type:", node.type);
+
+    // const startPosition = node.startPosition;
+    // const endPosition = node.endPosition;
+    // const chunk = code
+    //   .split("\n")
+    //   .slice(startPosition.row, endPosition.row + 1);
+    // const chunkCode = chunk.join("\n");
+
+    // console.log(
+    //   "\n" + 'NODE TYPE: "' + node.type + '"' + "\n" + chunkCode + "\n"
+    // );
+
     if (listAllowedNodeTypes.includes(node.type)) {
       // GET THE CHUNK
       const startPosition = node.startPosition;
@@ -53,6 +66,8 @@ const findChunks = ({ node, code, path, languageNodes }: findChunksParams) => {
         .split("\n")
         .slice(startPosition.row, endPosition.row + 1);
       const chunkCode = chunk.join("\n");
+
+      // console.log('NODE TYPE: "' + node.type + '"', "chunkCode:", chunkCode);
 
       // CHECK IF CHUNK IS TOO LONG, IF SO, GO ONE LEVEL DEEPER.
       if (chunkCode.length > MAX_CHUNK_SIZE) {
@@ -66,10 +81,11 @@ const findChunks = ({ node, code, path, languageNodes }: findChunksParams) => {
           checkDuplicates.push(chunkCode);
           // IF CHUNK IS LARGE THAN 500 CHARACTERS, ADD IT TO THE CHUNK ARRAY.
           if (tempChunkLength > 500) {
-            chunkArray.push({
-              data: tempChunks,
-              file_name: path,
-            });
+            // chunkArray.push({
+            //   data: tempChunks,
+            //   file_name: path,
+            // });
+            chunkArray.push(tempChunks);
             tempChunks = "";
             tempChunkLength = 0;
             // IF IT IS SMALLER THAN 500, ADD IT TO tempChunks.
@@ -96,19 +112,21 @@ const findChunks = ({ node, code, path, languageNodes }: findChunksParams) => {
 
   // CHECK IF CHUNKS WERE FOUND IN A FILE BUT ARE UNDER 500 CHARACTERS AFTER THE ENTIRE FILE WAS TRAVERSED.
   if (tempChunks.length > 0) {
-    chunkArray.push({
-      data: tempChunks,
-      file_name: path,
-    });
+    // chunkArray.push({
+    //   data: tempChunks,
+    //   file_name: path,
+    // });
+    chunkArray.push(tempChunks);
   }
 
   // RETURN ALL OF THE CHUNKS FROM THE PROVIDED FILE.
-  const chunkFileObject = {
-    file_path: path,
-    data_chunks: chunkArray,
-  };
+  // const chunkFileData = {
+  //   file_path: path,
+  //   data_chunks: chunkArray,
+  // };
+  const chunkFileData = chunkArray;
 
-  return chunkFileObject;
+  return chunkFileData;
   // return node.rootNode;
 };
 
